3fc1f4925d4e499d25b98ce9f17825a482f82fc7
MisterJulsen <geisterdrache02@gmail.com>
2024-06-19 08:49:08 +0200

    Removed unused config

--- common/src/main/java/de/mrjulsen/dragnsounds/config/ClientConfig.java
+++ common/src/main/java/de/mrjulsen/dragnsounds/config/ClientConfig.java
@@ -7,7 +7,6 @@ public class ClientConfig {
     public static final ForgeConfigSpec SPEC;
 
     
-    public static final ForgeConfigSpec.ConfigValue<Boolean> PRINT_DEBUG_IN_CONSOLE;
     public static final ForgeConfigSpec.ConfigValue<Integer> MAX_STREAMING_CHANNELS;
 
 
@@ -15,8 +14,6 @@ public class ClientConfig {
         BUILDER.push("DragNSounds API Config");
 
         /* CONFIGS */
-        PRINT_DEBUG_IN_CONSOLE = BUILDER.comment("When active, will print the current state of all sound actions to the console. Let it enabled for better bug reporting, disable it if it spams your console too much.")
-            .define("print_debug", true);
         MAX_STREAMING_CHANNELS = BUILDER.comment("The amount of streaming sound channels. By default, Minecraft has 8 streaming channels, but this mod increases it to 32. (Restart required)")
             .defineInRange("max_streaming_channels", 32, 8, 255);
 
