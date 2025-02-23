e4ed9d82cc41f0a930d73a4c6fa37a915d1e4e96
MrJulsen <geisterdrache02@gmail.com>
2025-02-23 13:56:38 +0100

    Fixed crash on some linux systems

--- common/src/main/java/de/mrjulsen/dragnsounds/config/ClientConfig.java
+++ common/src/main/java/de/mrjulsen/dragnsounds/config/ClientConfig.java
@@ -7,15 +7,10 @@ public class ClientConfig {
     public static final ForgeConfigSpec SPEC;
 
     
-    public static final ForgeConfigSpec.ConfigValue<Integer> MAX_STREAMING_CHANNELS;
-
 
     static {
         BUILDER.push("DragNSounds API Config");
 
-        /* CONFIGS */
-        MAX_STREAMING_CHANNELS = BUILDER.comment("The amount of streaming sound channels. By default, Minecraft has 8 streaming channels, but this mod increases it to 32. (Restart required)")
-            .defineInRange("max_streaming_channels", 32, 8, 255);
 
         BUILDER.pop();
         SPEC = BUILDER.build();
